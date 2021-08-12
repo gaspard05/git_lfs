15f8db4143c402ef3945aeb4868dc483a6a117c7
gaspard05 <gnzamura@hotmail.com>
2021-08-12 15:21:47 +0100

    Git LFS testing

--- WebApplication1/WebApplication1/Startup.cs
+++ WebApplication1/WebApplication1/Startup.cs
@@ -18,6 +18,7 @@ namespace WebApplication1
             Configuration = configuration;
         }
 
+        //Comment for testing LFS settting
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
